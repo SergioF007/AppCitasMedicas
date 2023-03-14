663a7cc5b701cef694f53bb9d4f5584beebd2301
SergioFS_Ing07 <sefernandez0711@gmail.com>
2023-03-13 23:16:03 -0500

    añado apedillo a nombre del doctor

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         Doctor myDoctor = new Doctor("Sergio Fernández Salazar");
-        myDoctor.name = "Sergio Fernandez";
+        myDoctor.name = "Sergio Fernandez Salazar";
         myDoctor.showName();
     }
 }
