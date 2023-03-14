5a60674dab8e59aae04aeeaa926c205d807a05f8
SergioFS_Ing07 <sefernandez0711@gmail.com>
2023-03-14 16:26:05 -0500

    Cracion de la lista Constante MONTHS y aplicando accion de Opcion Book an appointments

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,6 @@ import static ui.UIMenu.*;
 public class Main {
     public static void main(String[] args) {
 
-        Doctor myDoctor = new Doctor();
-        myDoctor.name = "Sergio Fernandez Salazar";
-        myDoctor.showName();
-        myDoctor.showID();
-
-        Doctor myDoctor1 = new Doctor();
-        myDoctor1.showID();
-
         showMenu();
     }
 }
--- src/ui/UIMenu.java
+++ src/ui/UIMenu.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 public class UIMenu {
 
+    public static final String[] MONTHS ={"Enero", "Febrero", "Marzo", "Abril", "Mayo","Junio","Julio","Septiembre","Octubre","Noviembre","Diciembre" };
+
     public static void showMenu() {
         System.out.println("Welcome to My Appointments");
         System.out.println("Select the option you want");
@@ -50,6 +52,9 @@ public class UIMenu {
             switch (response) {
                 case 1:
                     System.out.println(":: Book an appointment");
+                    for (int i = 1; i < 4; i++) {
+                        System.out.println(i+ ". " + MONTHS[i-1] );
+                    }
                     break;
                 case 2:
                     System.out.println(":: My appointment");
