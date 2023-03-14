f259d686c61f7b09c7abfc3b0d7e208eb7a0d0af
SergioFS_Ing07 <sefernandez0711@gmail.com>
2023-03-13 23:11:53 -0500

    Creo dos tipo de constructor en la clase doctor y el la clase main tengo uso estos dos contructores para crear el objeto doctor

--- src/Doctor.java
+++ src/Doctor.java
@@ -4,6 +4,14 @@ public class Doctor {
     String name;
     String speciality;
 
+    public Doctor() {
+        System.out.println("Contruyendo el Objeto Doctor");
+    }
+
+    public Doctor(String name) {
+        System.out.println("EL nombre del Doctor asignado es: " + name);
+    }
+
     //Comportamientos
     public void showName(){
         System.out.println(name);
--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import javax.print.Doc;
 public class Main {
     public static void main(String[] args) {
 
-        Doctor myDoctor = new Doctor();
+        Doctor myDoctor = new Doctor("Sergio Fernández Salazar");
         myDoctor.name = "Sergio Fernandez";
         myDoctor.showName();
     }
