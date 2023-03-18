e0916af5b219218ebe5a067cd8ce78a1f6f6ee60
SergioFS_Ing07 <sefernandez0711@gmail.com>
2023-03-17 22:26:42 -0500

    aplico encapsulamieto con private y creo los setter y getter de la clase patient, implementando modificaciones en los metodos set de weight y height

--- src/Main.java
+++ src/Main.java
@@ -9,19 +9,12 @@ public class Main {
 
         // creo un Doctor
         Doctor myDoctor = new Doctor("Jose Ignacio Fernández", "Cardiologia");
-        System.out.println(myDoctor.name);
-        System.out.println(myDoctor.speciality);
+
 
         // creo un paciente
         Patient patient = new Patient("Jame Rodrigez", "jame10@gmail.com");
-        System.out.println(patient.name);
-        System.out.println(patient.email);
 
-        patient.weight = 65; // Kg
-        patient.height = 1.80; // m
 
-        System.out.println(patient.weight);
-        System.out.println(patient.height);
 
 
     }
--- src/ui/Patient.java
+++ src/ui/Patient.java
@@ -2,19 +2,82 @@ package ui;
 
 public class Patient {
 
-    static int id = 0;
-    public String name;
-    public String email;
+    int id = 0;
+    private String name;
+    private String email;
     private String address;
     private String phoneNumber;
     private String birthday;
-    public double weight;
-    public double height;
+    private double weight;
+    private double height;
     private String blood;
 
 
     public Patient(String name, String email) {
         this.name = name;
         this.email = email;
+    }
+
+    public void setWeight(double weight){
+        this.weight=weight;
+    }
+    public String getWeight() {
+        return this.weight + " Kg.";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public String setHeight(double height) {
+        return height + " Mts.";
+    }
+
+    public String getBlood() {
+        return blood;
+    }
+
+    public void setBlood(String blood) {
+        this.blood = blood;
     }
 }
