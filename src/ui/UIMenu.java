7ed7a940cc68066e9b9d724f2886a25b4b7e6086
SergioFS_Ing07 <sefernandez0711@gmail.com>
2023-03-23 15:54:28 -0500

    Creamos el metodo authUser para hacer una autenticacion de los usuarios Doctor y Patient y dentro de este creamos 2 tipos de lista(doctors y patients) donde crearemos 3 objetos de cada objeto para tener con quien validar

--- src/ui/UIMenu.java
+++ src/ui/UIMenu.java
@@ -1,17 +1,24 @@
 package ui;
 
+import Modelo.Doctor;
+import Modelo.Patient;
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class UIMenu {
 
     public static final String[] MONTHS ={"Enero", "Febrero", "Marzo", "Abril", "Mayo","Junio","Julio","Septiembre","Octubre","Noviembre","Diciembre" };
 
+    public static Doctor doctorLogeed;
+
+    public static Patient patientLogged;
     public static void showMenu() {
         System.out.println("Welcome to My Appointments");
         System.out.println("Select the option you want");
         int response = 0;
         do {
-            System.out.println("1. Modelo.Doctor");
+            System.out.println("1. Doctor");
             System.out.println("2. Patient");
             System.out.println("0. Exit");
 
@@ -20,11 +27,13 @@ public class UIMenu {
 
             switch (response) {
                 case 1:
-                    System.out.println("Modelo.Doctor");
+                    System.out.println("Doctor");
+                    authUser(1); // Ejecuta la validacion comparandolo con la lista doctors
+                    response = 0;
                     break;
                 case 2:
                     System.out.println("Patient");
-                    showPatientMenu();
+                    authUser(2);
                     response = 0;
                 case 0:
                     System.out.println("Thank you for you visit");
@@ -33,6 +42,53 @@ public class UIMenu {
                     System.out.println("Please select a correct answer");
             }
         }while (response != 0);
+    }
+
+    //metodo autenticaiones  authUser
+    // Con el Fin de saber quien quien es Doctor o Patient
+    // vamos hacer la simularicon
+
+    private static void authUser(int userType) {
+        //Docotor userType = 1
+        //Patien userType = 2
+        ArrayList<Doctor> docotrs = new ArrayList<>();
+        docotrs.add(new Doctor("Sergio Fernandez Salazar", "sfs@gamil.com"));
+        docotrs.add(new Doctor("Sebastian ramon", "seb@gamil.com"));
+        docotrs.add(new Doctor("Camilo Rodrigrez", "cam@gamil.com"));
+
+        ArrayList<Patient> patients = new ArrayList<>();
+        patients.add(new Patient("Jose Fernandez", "jse@gamil.com"));
+        patients.add(new Patient("Paula fernanda", "ptf@gamil.com"));
+        patients.add(new Patient("Daniel Gomez", "dni@gamil.com"));
+
+        boolean emailCorrect = false;
+        do {
+            System.out.println("Insert your email: [a@a.com]");
+            Scanner sc = new Scanner(System.in);
+            String email = sc.nextLine();
+
+            if (userType == 1) {
+
+                for (Doctor d: docotrs) {
+                    if(d.getEmail().equals(email)) {
+                        emailCorrect = true;
+                        //Obtener el ususario logeado
+                        doctorLogeed = d;
+                        //showDoctorMenu
+                    }
+                }
+
+            } else if (userType == 2) {
+                for (Patient p : patients) {
+                    if(p.getEmail().equals(email)) {
+                        emailCorrect = true;
+                        patientLogged = p;
+                    }
+                }
+            }
+
+        } while(!emailCorrect);
+
     }
 
     static void showPatientMenu() {
